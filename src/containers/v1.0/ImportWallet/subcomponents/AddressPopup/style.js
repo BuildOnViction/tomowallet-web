1f2f471c148aeb0d491b035f83294255ef4c43fc
xiemnguyen <xiem.bg@gmail.com>
2019-08-21 11:27:32 +0700

    style popup "Ledger Wallet"

--- src/containers/v1.0/ImportWallet/subcomponents/AddressPopup/style.js
+++ src/containers/v1.0/ImportWallet/subcomponents/AddressPopup/style.js
@@ -8,10 +8,23 @@
 import styled from 'styled-components';
 // Custom Components
 import Popup from '../../../../../components/Popup';
+import { Input } from 'reactstrap';
 // ===================
 
 // ===== STYLE =====
 const AddressPopupStyler = styled(Popup)``;
+
+const InputStylerRadio = styled(Input)`
+    position: absolute;
+    margin-top: 0;
+    margin-left: 0;
+    left: 5px;
+    top: 4px;
+`;
+
 // =================
 
-export { AddressPopupStyler };
+export {
+  AddressPopupStyler,
+  InputStylerRadio,
+};
