accc78bd4320ccff4a7ebf1fd13e6ce6dc4c3835
xiemnguyen <xiem.bg@gmail.com>
2019-08-26 16:08:12 +0700

    Check responsive: Not support notice

--- src/containers/MyWallet/subcomponents/TransactionTable/style.js
+++ src/containers/MyWallet/subcomponents/TransactionTable/style.js
@@ -5,6 +5,9 @@ const BoxTransaction = styled.div`
   .rt-tr {
     display: grid;
     grid-template-columns: 10% 20% 15% 20% 5% 20% 10%;
+    @media (max-width: 991px) {
+      grid-template-columns: 10% 18% 15% 18% 5% 18% 14%;
+    }
   }
 `;
 
