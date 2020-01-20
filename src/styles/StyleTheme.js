fbc3d4ac2d6ac36f978c621f3ae63cf547d30510
thanhvk <thanhvk.w3s@gmail.com>
2020-01-20 18:08:08 +0700

    style footer for two modes

--- src/styles/StyleTheme.js
+++ src/styles/StyleTheme.js
@@ -4,12 +4,18 @@ const theme = {
         navBackground: '#272D40',
         boxBackground: '#fff',
         modalColor: '#202533',
+        footerColor: '#A5A8AD',
+        footerSocialBackground: '#fff',
+        footerSocialHoverColor: '#202533',
     },
     incognito: {
         background: '#151515',
         navBackground: '#1C1C1C',
         boxBackground: '#1C1C1C',
         modalColor: '#fff',
+        footerColor: '#757575',
+        footerSocialBackground: '#1C1C1C',
+        footerSocialHoverColor: '#fff',
     }
 };
 
