22fa14eeb5f733d085f7fff1e5f5d39a48d28bbd
pqv199x <pqv199x@hotmail.com>
2020-02-19 11:48:49 +0700

    update privacy transaction table

--- src/containers/MyWallet/subcomponents/TransactionTable/privacy-config.js
+++ src/containers/MyWallet/subcomponents/TransactionTable/privacy-config.js
@@ -20,105 +20,85 @@ import { MSG, API } from '../../../../constants';
 
 // ===== CONFIGURATION =====
 export default ({ formatMessage }) => [
-  {
-    Header: () => (
-      <Fragment>
-        <FontAwesomeIcon icon='hashtag' className='mr-2' />
-        {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_TX_HASH)}
-      </Fragment>
-    ),
-    accessor: TRANSACTION_COLUMNS.TX_HASH,
-    Cell: ({ value }) => (
-      <TextBlue>
-        <a
-          href={`${_get(API, [getNetwork(), 'VIEW_TRANSACTION'])}/${value}`}
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <Ellipsis middle>{value}</Ellipsis>
-        </a>
-      </TextBlue>
-    ),
-  },
-  {
-    Header: () => (
-      <Fragment>
-        <FontAwesomeIcon icon='history' className='mr-2' />
-        {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_CREATE_TIME)}
-      </Fragment>
-    ),
-    accessor: TRANSACTION_COLUMNS.CREATE_TIME,
-    Cell: ({ value }) => {
-      const timeStr = value.format('HH:mm, DD MMM YYYY');
-      return <Ellipsis>{timeStr}</Ellipsis>;
+    {
+        Header: () => (
+        <Fragment>
+            <FontAwesomeIcon icon='history' className='mr-2' />
+            {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_CREATE_TIME)}
+        </Fragment>
+        ),
+        accessor: TRANSACTION_COLUMNS.CREATE_TIME,
+        Cell: ({ value }) => {
+        const timeStr = value.format('HH:mm, DD MMM YYYY');
+        return <Ellipsis>{timeStr}</Ellipsis>;
+        },
     },
-  },
-  {
-    Header: () => (
-      <Fragment>
-        <FontAwesomeIcon icon={['far', 'address-card']} className='mr-2' />
-        {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_FROM)}
-      </Fragment>
-    ),
-    accessor: TRANSACTION_COLUMNS.FROM,
-    Cell: ({ value }) => (
-      <TextBlue>
-        <a
-          href={`${_get(API, [getNetwork(), 'VIEW_ADDRESS'])}/${value}`}
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <Ellipsis middle>{value}</Ellipsis>
-        </a>
-      </TextBlue>
-    ),
-  },
-  {
-    Header: '',
-    headerClassName: 'no-header',
-    accessor: 'connectArrow',
-    Cell: ({ original }) =>
-      _get(original, [TRANSACTION_COLUMNS.TYPE]) === 'IN' ? (
-        <FontAwesomeIcon icon='arrow-right' className='text-success' />
-      ) : (
-        <FontAwesomeIcon icon='arrow-right' className='text-danger' />
-      ),
-    className: 'text-center',
-  },
-  {
-    Header: () => (
-      <Fragment>
-        <FontAwesomeIcon icon={['far', 'address-card']} className='mr-2' />
-        {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_TO)}
-      </Fragment>
-    ),
-    accessor: TRANSACTION_COLUMNS.TO,
-    Cell: ({ value }) => (
-      <TextBlue>
-        <a
-          href={`${_get(API, [getNetwork(), 'VIEW_ADDRESS'])}/${value}`}
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <Ellipsis middle>{value}</Ellipsis>
-        </a>
-      </TextBlue>
-    ),
-  },
-  {
-    Header: () => (
-      <Fragment>
-        <FontAwesomeIcon icon='funnel-dollar' className='mr-2' />
-        {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_QUANTITY)}
-      </Fragment>
-    ),
-    accessor: TRANSACTION_COLUMNS.QUANTITY,
-    Cell: ({ value, original }) => {
-      const displayValue = `${value} ${_get(original, [
-          TRANSACTION_COLUMNS.TOKEN_TYPE,
-        ])}`;
-      return <Ellipsis>{displayValue}</Ellipsis>;
+    {
+        Header: () => (
+        <Fragment>
+            <FontAwesomeIcon icon={['far', 'address-card']} className='mr-2' />
+            {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_FROM)}
+        </Fragment>
+        ),
+        accessor: TRANSACTION_COLUMNS.FROM,
+        Cell: ({ value }) => (
+        <TextBlue>
+            <a
+            href={`${_get(API, [getNetwork(), 'VIEW_ADDRESS'])}/${value}`}
+            rel='noopener noreferrer'
+            target='_blank'
+            >
+            <Ellipsis middle>{value}</Ellipsis>
+            </a>
+        </TextBlue>
+        ),
     },
-  },
+    {
+        Header: '',
+        headerClassName: 'no-header',
+        accessor: 'connectArrow',
+        Cell: ({ original }) =>
+        _get(original, [TRANSACTION_COLUMNS.TYPE]) === 'IN' ? (
+            <FontAwesomeIcon icon='arrow-right' className='text-success' />
+        ) : (
+            <FontAwesomeIcon icon='arrow-right' className='text-danger' />
+        ),
+        className: 'text-center',
+    },
+    {
+        Header: () => (
+        <Fragment>
+            <FontAwesomeIcon icon={['far', 'address-card']} className='mr-2' />
+            {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_TO)}
+        </Fragment>
+        ),
+        accessor: TRANSACTION_COLUMNS.TO,
+        Cell: ({ value }) => (
+        <TextBlue>
+            <a
+            href={`${_get(API, [getNetwork(), 'VIEW_ADDRESS'])}/${value}`}
+            rel='noopener noreferrer'
+            target='_blank'
+            >
+            <Ellipsis middle>{value}</Ellipsis>
+            </a>
+        </TextBlue>
+        ),
+    },
+    {
+        Header: () => (
+        <Fragment>
+            <FontAwesomeIcon icon='funnel-dollar' className='mr-2' />
+            {formatMessage(MSG.MY_WALLET_TABLE_TRANSACTIONS_HEADER_QUANTITY)}
+        </Fragment>
+        ),
+        accessor: TRANSACTION_COLUMNS.QUANTITY,
+        Cell: ({ value, original }) => {
+        const displayValue = `${value} ${_get(original, [
+            TRANSACTION_COLUMNS.TOKEN_TYPE,
+            ])}`;
+        return <Ellipsis>{displayValue}</Ellipsis>;
+        },
+    },
 ];
 // =========================
--- src/containers/MyWallet/subcomponents/TransactionTable/style.js
+++ src/containers/MyWallet/subcomponents/TransactionTable/style.js
@@ -35,6 +35,40 @@ const BoxTransaction = styled.div`
   }
 `;
 
+const BoxPrivacyTransaction = styled.div`
+  .rt-tr {
+    display: grid;
+    grid-template-columns: 20% 30% 5% 30% 15%;
+    .rt-th {
+      position: relative;
+      &:not(.no-header):before {
+        content '';
+        position: absolute;
+        bottom: 0;
+        left: 2%;
+        width: 20px;
+        height: 0;
+        border-left: 10px solid ${props => props.theme.tableHeaderCellColor};
+        border-bottom: 3px solid ${props => props.theme.tableHeaderCellColor};
+        border-top: 3px solid transparent;
+        border-right: 10px solid transparent;
+      }
+      &:after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 2%;
+        width: 96%;
+        height: 1px;
+        background-color: ${props => props.theme.tableHeaderCellColor};
+      }
+    }
+    @media (max-width: 991px) {
+      grid-template-columns: 20% 17% 20% 7% 20% 16%;
+    }
+  }
+`;
+
 // =================
 
-export { BoxTransaction };
+export { BoxTransaction, BoxPrivacyTransaction };
