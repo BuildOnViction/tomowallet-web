4ced0c36c69191c4fc0f2032ecc124a77e308c67
Lê Nam Hùng <hungle@tomochain.com>
2020-02-27 18:10:42 +0700

    Change terms & policy redirect link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,14 +8,14 @@
  */
 // ===== IMPORTS =====
 // Modules
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { Row, Col, Nav, NavItem } from 'reactstrap';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { Row, Col, Nav, NavItem } from "reactstrap";
 // Custom Component
-import { LinkFooter, TextGray } from './style';
+import { LinkFooter, TextGray } from "./style";
 // Utilities & Constants
-import { withIntl } from '../IntlProvider';
-import { MSG } from '../../constants';
+import { withIntl } from "../IntlProvider";
+import { MSG } from "../../constants";
 // ===================
 
 // ===== MAIN COMPONENT =====
@@ -23,73 +23,73 @@ class Footer extends PureComponent {
   constructor(props) {
     super(props);
     const {
-      intl: { formatMessage },
+      intl: { formatMessage }
     } = props;
 
     this.SOCIAL_NETWORKS = [
       {
-        className: 'font-icon-facebook',
-        url: 'https://www.facebook.com/tomochainofficial',
+        className: "font-icon-facebook",
+        url: "https://www.facebook.com/tomochainofficial"
       },
       {
-        className: 'font-icon-twitter',
-        url: 'https://twitter.com/TomoChainANN',
+        className: "font-icon-twitter",
+        url: "https://twitter.com/TomoChainANN"
       },
       {
-        className: 'font-icon-telegram',
-        url: 'https://t.me/tomochain',
+        className: "font-icon-telegram",
+        url: "https://t.me/tomochain"
       },
       {
-        className: 'font-icon-github',
-        url: 'https://github.com/tomochain/',
+        className: "font-icon-github",
+        url: "https://github.com/tomochain/"
       },
       {
-        className: 'font-icon-linkedin',
-        url: 'https://www.linkedin.com/company/tomochain',
+        className: "font-icon-linkedin",
+        url: "https://www.linkedin.com/company/tomochain"
       },
       {
-        className: 'font-icon-reddit',
-        url: 'https://www.reddit.com/r/Tomochain/',
-      },
+        className: "font-icon-reddit",
+        url: "https://www.reddit.com/r/Tomochain/"
+      }
     ];
     this.WEBSITE_SUPPORTED_LINKS = [
       {
-        url: 'https://docs.tomochain.com/products/tomowallet/features/',
-        content: formatMessage(MSG.FOOTER_OPTION_HELP),
+        url: "https://docs.tomochain.com/products/tomowallet/features/",
+        content: formatMessage(MSG.FOOTER_OPTION_HELP)
       },
       {
-        url: 'https://docs.tomochain.com/products/tomowallet/terms/',
-        content: formatMessage(MSG.FOOTER_OPTION_TERMS_PRIVACY),
+        url: "https://tomochain.gitbook.io/tomochain-docs/legal/term-of-use",
+        content: formatMessage(MSG.FOOTER_OPTION_TERMS_PRIVACY)
       },
       {
-        url: 'https://tomochain.com',
-        content: formatMessage(MSG.HEADER_NAVBAR_OPTION_ABOUT),
+        url: "https://tomochain.com",
+        content: formatMessage(MSG.HEADER_NAVBAR_OPTION_ABOUT)
       },
       {
-        url: 'https://docs.tomochain.com/general/faq/#tomowallet',
-        content: formatMessage(MSG.HEADER_NAVBAR_OPTION_FAQS),
-      },
+        url: "https://docs.tomochain.com/general/faq/#tomowallet",
+        content: formatMessage(MSG.HEADER_NAVBAR_OPTION_FAQS)
+      }
     ];
   }
 
   render() {
     const {
-      intl: { formatMessage },
+      intl: { formatMessage }
     } = this.props;
     return (
-      <Row className='align-items-center pt-3 pb-3'>
+      <Row className="align-items-center pt-3 pb-3">
         <Col xs={12} md={7}>
-          <TextGray className='mb-text-center'>
+          <TextGray className="mb-text-center">
             {formatMessage(MSG.FOOTER_VERSION_TEXT)}
           </TextGray>
-          <Row className='footer-menu'>
+          <Row className="footer-menu">
             <Nav>
               {this.WEBSITE_SUPPORTED_LINKS.map((link, linkIdx) => (
                 <NavItem key={`website_link_${linkIdx + 1}`}>
                   <LinkFooter
                     href={link.url}
-                    rel='noopener noreferrer'
-                    target='_blank'
+                    rel="noopener noreferrer"
+                    target="_blank"
                   >
                     {link.content}
                   </LinkFooter>
@@ -99,13 +99,13 @@ class Footer extends PureComponent {
           </Row>
         </Col>
         <Col xs={12} md={5}>
-          <Nav className='footer-buttons'>
+          <Nav className="footer-buttons">
             {this.SOCIAL_NETWORKS.map((item, itemIdx) => (
               <NavItem key={`footer_button_${itemIdx + 1}`}>
                 <LinkFooter
                   href={item.url}
-                  rel='noopener noreferrer'
-                  target='_blank'
+                  rel="noopener noreferrer"
+                  target="_blank"
                 >
                   {<i className={item.className} />}
                 </LinkFooter>
@@ -122,11 +122,11 @@ class Footer extends PureComponent {
 // ===== PROP TYPES =====
 Footer.propTypes = {
   /** React Intl's instance object */
-  intl: PropTypes.object,
+  intl: PropTypes.object
 };
 
 Footer.defaultProps = {
-  intl: {},
+  intl: {}
 };
 // ======================
 
