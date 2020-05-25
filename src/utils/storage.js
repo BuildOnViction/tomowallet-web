4150a73fb93d8d3a1509bad4b92cd5e4b1120e18
Lê Nam Hùng <hungle@tomochain.com>
2020-05-25 11:48:00 +0700

    Store locale in local storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -5,53 +5,67 @@
  */
 // ===== IMPORTS =====
 // Modules
-import _get from 'lodash.get';
-import _omit from 'lodash.omit';
-import CryptoJS from 'crypto-js';
+import _get from "lodash.get";
+import _omit from "lodash.omit";
+import CryptoJS from "crypto-js";
 // ===================
 
 // ===== METHODS =====
 const setStorage = (key, object) => {
   sessionStorage.setItem(
-    'global',
+    "global",
     JSON.stringify({
-      ...JSON.parse(sessionStorage.getItem('global')),
+      ...JSON.parse(sessionStorage.getItem("global")),
       [key]: object,
-    }),
+    })
   );
 };
-const getStorage = key => {
-  return _get(JSON.parse(sessionStorage.getItem('global')), [key]);
+const getStorage = (key) => {
+  return _get(JSON.parse(sessionStorage.getItem("global")), [key]);
 };
-const removeStorage = key => {
+const removeStorage = (key) => {
   sessionStorage.setItem(
-    'global',
-    JSON.stringify(_omit(JSON.parse(sessionStorage.getItem('global')), key)),
+    "global",
+    JSON.stringify(_omit(JSON.parse(sessionStorage.getItem("global")), key))
   );
 };
 
-export const setWeb3Info = web3Info => {
+const setGlobalStorage = (key, object) => {
+  localStorage.setItem(key, JSON.stringify(object));
+};
+const getGlobalStorage = (key) => {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch {
+    return localStorage.getItem(key);
+  }
+};
+const removeGlobalStorage = (key) => {
+  localStorage.removeItem(key);
+};
+
+export const setWeb3Info = (web3Info) => {
   let infoToStore = {
     ...web3Info,
   };
-  if (_get(web3Info, 'recoveryPhrase')) {
+  if (_get(web3Info, "recoveryPhrase")) {
     const encryptedRP = CryptoJS.AES.encrypt(
       web3Info.recoveryPhrase,
-      'ni@hC0m0T',
+      "ni@hC0m0T"
     ).toString();
     infoToStore.recoveryPhrase = encryptedRP;
   }
 
-  setStorage('web3Info', infoToStore);
+  setStorage("web3Info", infoToStore);
 };
 export const getWeb3Info = () => {
-  const web3Info = getStorage('web3Info');
-  const recoveryPhrase = _get(web3Info, 'recoveryPhrase');
+  const web3Info = getStorage("web3Info");
+  const recoveryPhrase = _get(web3Info, "recoveryPhrase");
 
   if (recoveryPhrase) {
     const decryptedRP = CryptoJS.AES.decrypt(
       recoveryPhrase,
-      'ni@hC0m0T',
+      "ni@hC0m0T"
     ).toString(CryptoJS.enc.Utf8);
     return {
       ...web3Info,
@@ -61,17 +75,17 @@ export const getWeb3Info = () => {
 
   return web3Info;
 };
-export const removeWeb3Info = () => removeStorage('web3Info');
+export const removeWeb3Info = () => removeStorage("web3Info");
 
-export const setLocale = locale => setStorage('locale', locale);
-export const getLocale = () => getStorage('locale');
-export const removeLocale = () => removeStorage('locale');
+export const setLocale = (locale) => setGlobalStorage("locale", locale);
+export const getLocale = () => getGlobalStorage("locale");
+export const removeLocale = () => removeGlobalStorage("locale");
 
-export const setNetwork = network => setStorage('network', network);
-export const getNetwork = () => getStorage('network');
-export const removeNetwork = () => removeStorage('network');
+export const setNetwork = (network) => setStorage("network", network);
+export const getNetwork = () => getStorage("network");
+export const removeNetwork = () => removeStorage("network");
 
-export const setLedger = ledger => setStorage('ledger', ledger);
-export const getLedger = () => getStorage('ledger');
-export const removeLedger = () => removeStorage('ledger');
+export const setLedger = (ledger) => setStorage("ledger", ledger);
+export const getLedger = () => getStorage("ledger");
+export const removeLedger = () => removeStorage("ledger");
 // ===================
