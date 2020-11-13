196e294c0cdf134f077fa6afdbebd034a1740b47
anhnt <anhntbk08@gmail.com>
2020-11-13 14:27:14 +0700

    [Chore] add wasm on prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const common = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = common({
   mode: 'production',
@@ -25,6 +26,9 @@ module.exports = common({
   },
   plugins: [
     new webpack.IgnorePlugin(/^\.\/(?!english)/, /bip39\/src\/wordlists$/),
+    new CopyWebpackPlugin(
+      [{ from: 'public'}],
+    ),
   ],
   performance: {
     maxEntrypointSize: 512000,
